19e200f595cb0680142ebe02c890c13a0e383e8a
MarthinJConterasR <marj_contrerasr@unicah.edu>
2025-04-10 13:08:30 -0600

    Mi primer Commit desde Visual Studio Code

--- conexion.php
+++ conexion.php
@@ -1,7 +1,30 @@
 <?php
 $conexion = new PDO(
+<<<<<<< HEAD
+=======
+    "mysql:dbname=gastosfamilia;127.0.0.1,", 
+    "root",                          
+    ""                                
+);
+?>
+
+<!--
+Datos de la bdd gastos:
+nombre:u768712027_bdd_gastos
+usuario:u768712027_userGastos
+host:congresoicc.com
+contraseña:GastosProgramacionNegocios123456789.
+-->
+
+<!-- 
+$conexion = new PDO(
+>>>>>>> 8e2d089 (Mi primer Commit desde Visual Studio Code)
     "mysql:u768712027_bdd_gastos;127.0.0.1",
     "u768712027_userGastos",
     "GastosProgramacionNegocios123456789."
 );
+<<<<<<< HEAD
 ?>
+=======
+-->
+>>>>>>> 8e2d089 (Mi primer Commit desde Visual Studio Code)
