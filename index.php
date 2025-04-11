9b354be141c600315f6c92dc50e833abb4909f91
MarthinJConterasR <marj_contrerasr@unicah.edu>
2025-04-10 20:38:56 -0600

    Creacion en Php de: Registro Nuevo, Modificacion de un registro ya existente, obtencion de un registro para Modificar o eliminar, La posibilidad de modificar un registro y tambien un boton para Guardar, validaciones de errores al momento de llenar los campos en el HTML.

--- index.php
+++ index.php
@@ -10,6 +10,97 @@ $tipoGasto= "";
 $valorGasto = "";       
 $codigoGasto = null;  
 
+//Insertar un registro con el Boton Enviar
+if(isset($_REQUEST['submit1'])){
+    $hay_post = true;
+    $nombre = isset($_REQUEST['txtNombre']) ? $_REQUEST['txtNombre'] : "";
+    $tipoGasto = isset($_REQUEST['tipoGasto']) ? $_REQUEST['tipoGasto'] : "";
+    $valorGasto = isset($_REQUEST['valorGasto']) ? $_REQUEST['valorGasto'] : "";
+
+    if(!empty($nombre)){
+        $nombre = preg_replace("/[^a-zA-ZáéíóúÁÉÍÓÚ ]/u","",$nombre);
+    } else {
+        $error .= "El nombre no puede estar vacío<br>";
+    }
+
+    if($tipoGasto == ""){
+        $error .= "Seleccione un tipo de Gasto para poder continuar.<br>";
+    }
+    
+    if($valorGasto==""){
+        if (!is_numeric($valorGasto) || $valorGasto <= 0) {
+            $error .= "Introduzca un valor numérico positivo de gasto<br>";
+        }        
+    }
+
+    if(!$error){
+        $stm_insertarRegistro = $conexion->prepare("insert into gastos(nombre, tipoGasto, valorGasto) values(:nombre, :tipoGasto, :valorGasto)");
+        $stm_insertarRegistro->execute([':nombre'=>$nombre, ':tipoGasto'=>$tipoGasto, ':valorGasto'=>$valorGasto]);
+        header("Location: index.php?mensaje=registroGastoFamiliarGuardado");
+        exit();
+    }
+}
+
+//Modificando un Registro
+if(isset($_REQUEST['submit2'])){
+    $hay_post = true;
+    $nombre = isset($_REQUEST['txtNombre']) ? $_REQUEST['txtNombre'] : "";
+    $tipoGasto = isset($_REQUEST['tipoGasto']) ? $_REQUEST['tipoGasto'] : "";
+    $valorGasto = isset($_REQUEST['valorGasto']) ? $_REQUEST['valorGasto'] : "";
+
+    if(!empty($nombre)){
+        $nombre = preg_replace("/[^a-zA-ZáéíóúÁÉÍÓÚ ]/u","",$nombre);
+    } else {
+        $error .= "El nombre no puede estar vacío<br>";
+    }
+
+    if($tipoGasto == ""){
+        $error .= "Seleccione un tipo de Gasto para poder continuar.<br>";
+    }
+    
+    if($valorGasto==""){
+        if (!is_numeric($valorGasto) || $valorGasto <= 0) {
+            $error .= "Introduzca un valor numérico positivo de gasto<br>";
+        }        
+    }
+
+    if(!$error){
+        $stm_modificar = $conexion->prepare("update gastos set nombre = :nombre, tipoGasto = :tipoGasto, valorGasto = :valorGasto where codigoGasto = :id");
+        $stm_modificar->execute([
+            ':nombre'=>$nombre, 
+            ':tipoGasto'=>$tipoGasto, 
+            ':valorGasto'=>$valorGasto,
+            ':id'=> $codigoGasto
+        ]);
+        header("Location: index.php?mensaje=registroModificado");
+        exit();
+    }
+
+}
+
+//Modificar o Elimar un Registro
+if(isset($_REQUEST['id']) && isset($_REQUEST['op'])){
+    $id = $_REQUEST['id'];
+    $op = $_REQUEST['op'];
+    
+    if($op == 'm'){
+        $stm_seleccionarRegistro = $conexion->prepare("select * from gastos where codigoGasto=:id");
+        $stm_seleccionarRegistro->execute([':id'=>$id]);
+        $resultado = $stm_seleccionarRegistro->fetch();
+        $codigoGasto = $resultado['codigoGasto'];
+        $nombre = $resultado['nombre'];
+        $tipoGasto = $resultado['tipoGasto'];
+        $valorGasto = $resultado['valorGasto'];
+    }
+    else if($op == 'e'){
+        $stm_eliminar = $conexion->prepare("delete from gastos where codigoGasto = :id");
+        $stm_eliminar->execute([':id'=>$id]);
+        header("Location: index.php?mensaje=registroEliminado");
+        exit();
+    }
+}
+
+//
 
 
 
@@ -49,11 +140,11 @@ $codigoGasto = null;
             </select><br>
 
             <label class="form-label" for="valorGasto">Valor del Gasto</label>
-            <input class="form-control" type="number" name="valorGasto" id="ValorGasto" value="<?php echo "$ " . $valorGasto ?>"><br>
+            <input class="form-control" type="number" name="valorGasto" id="valorGasto" step="0.01" min="0" value="<?php echo "$ " . $valorGasto ?>"><br>
 
             <?php
                 if($codigoGasto){
-                    echo '<input class="btn btn-dark" type="submit" value="Modificar" name="submit2">';
+                    echo '<input class="btn btn-dark" type="submit" value="Guardar" name="submit2">';
                 } 
                 else {
                     echo '<input class="btn btn-primary" type="submit" value="Enviar" name="submit1">';
@@ -63,8 +154,17 @@ $codigoGasto = null;
         </form>
         <br>
 
-    </div>
+        <!-- VALIDACIONES DE ERRORES -->
+        <?php if($error): ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <?php echo "<p>$error</p>"; ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    <?php endif; ?>
 
-    
+
+
+
+    </div>
 </body>
 </html>
